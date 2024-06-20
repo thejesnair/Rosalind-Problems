dab0bb4648726ed95ba19df079cc2640396d8ac7
thejesnair <156398578+thejesnair@users.noreply.github.com>
2024-06-20 00:08:42 -0700

    Update Counting Point Mutations.py
    added additional code to use with dataset file

--- Counting Point Mutations.py
+++ Counting Point Mutations.py
@@ -1,3 +1,4 @@
+#test code with sampel set
 DNA1 = "GAGCCTACTAACGGGAT"
 DNA2 = "CATCGTAATGACGGCCT"
 
@@ -9,3 +10,13 @@ def mutation(string1, string2):
     return point_mutation
 
 print(mutation(DNA1, DNA2))
+
+#alternative code for dataset
+with open('rosalind_hamm_1_dataset.txt', 'r') as file:
+    point_mutation = 0
+    DNA1 = file.readline().strip() #strip leading and tailing white space
+    DNA2 = file.readline().strip()
+    for x,y in zip(DNA1, DNA2):
+        if x != y:
+            point_mutation += 1
+print(point_mutation)
