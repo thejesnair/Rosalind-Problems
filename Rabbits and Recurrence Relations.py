b1544ecd2f3514a418f14326a0a4054f91bdfe60
thejesnair <156398578+thejesnair@users.noreply.github.com>
2024-07-10 17:18:06 -0700

    Update Rabbits and Recurrence Relations.py
    replaced previous solution with newer/easier solution that utilizes traditional fib pattern and modifies it to fit a growing rabbit population

--- Rabbits and Recurrence Relations.py
+++ Rabbits and Recurrence Relations.py
@@ -6,15 +6,14 @@ https://miro.medium.com/v2/resize:fit:1400/format:webp/1*jPMKByWX0p7Q0zGzh_lzdw.
 https://miro.medium.com/v2/resize:fit:1400/format:webp/1*p4hQbk3Y3qvID17p-9Xtvg.png
 #illustrates pop growth after 5 months
 
-def rabbits_present(months, litter):
-    F1 = 1
-    F2 = 1 #constants
-    for n in range(months - 2): #rabbits reach reproductive age after one month 
-        F3 = F1*litter+F2 #1*3+1=4 rabbit pairs, starting one pair
-        #The number of offspring in any month is EQUAL to the number of rabbits that were alive two months prior.
-        F1 = F2 
-        F2 = F3
-    return F3
+#solution utilizes traditional fib sequence/pattern, easier to understand
+def fib_rabbits(months, offspring):
+    parent, child = 1,1
+    for int in range(months-1):
+        child, parent = parent, parent + (child*offspring) 
+        #multiply by offspring, beginning with 1 pair, producing litter of k
+        #rabbit pairs instead of 1 pair
+    return(child)
 
-print(rabbits_present(5,3))
+print(fib_rabbits(5,3))
 print(rabbits_present(32,3))
