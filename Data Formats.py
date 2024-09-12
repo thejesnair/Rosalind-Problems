32901cf86130e920feb65a50d213eb057b7b9e47
thejesnair <156398578+thejesnair@users.noreply.github.com>
2024-09-11 19:02:31 -0700

    Update Data Formats.py

--- Data Formats.py
+++ Data Formats.py
@@ -4,3 +4,5 @@ from Bio import SeqIO
 Entrez.email = ""
 handle = Entrez.efetch(db="nucleotide", id=["JX428803, JX308817, JQ342169, NM_204821, JX393321, BT149867, NM_001266228, JN573266, NM_001246828"], rettype="fasta")
 records = list(SeqIO.parse(handle, "fasta"))
+
+print(min(records, key=lambda s:len(s.seq)).format("fasta"))
